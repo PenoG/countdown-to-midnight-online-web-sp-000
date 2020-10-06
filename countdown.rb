1db6275b99b92f415314a96ae71b1ebbfec50295
Mason Phillips <mjphillips2000@gmail.com>
2020-10-06 19:59:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,11 +3,8 @@ number = 10
 
 def countdown(number)
   while number > 0
-    if number == 0
-      puts "HAPPY NEW YEARS!"
-    else
-      puts "#{number} SECOND(S)"
-    end
+    puts "#{number} SECOND(S)"
     number -= 1
   end
+  "HAPPY NEW YEAR!"
 end
